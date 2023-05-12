e3efd9b2a8cd33bd20e11b77e1994a1af8782cb9
Brandon Morgan <63347492+Bam0833@users.noreply.github.com>
2023-05-12 04:25:46 -0400

    Add files via upload

---
+++ # Instructions for CIS110 FINAL projext(.py
@@ -0,0 +1,27 @@
+# Instructions for CIS110 FINAL projext(Undercover_Professor)
+print("This is a story about a young pup who is forced with the choice of going undercover to save his owners grades")
+print("or runaway to a world he has never seen.")
+print("Listed is a series of questions, when you have your chosen input please press enter for each one.")
+print("Continue? Please press the ENTER key")
+
+keepPlaying = "yes"
+while keepPlaying == "no":
+    #Questions from the week 7 outline paper
+    userName = input("What is your name?         ")
+    dogName = input("What is the name of the dog?         ")
+    townName = input("Where is the setting of this story taking place?:      ")
+    underDog = input("What is the professors undercover name?       ")
+    toolName = input(" What super tool will the undercover professor use in which to convince the teacher the papers valid?         ")
+    postCardpup = input("What is the message for the owner on the post card at the end of the story??        ")
+#start of story
+keepPlaying = "yes"
+while (len(keepPlaying)) == "yes":
+userName = input("What is yout name?")
+if userName.lower() == "":
+ userName.lower() == input("Please enter yout name!        ")
+("Please enter a valid name!         ")
+
+userName.lower() == input(f"\nWelcome, {userName}, Lets get started!")
+dogName == input("Please tell me what the name of your dog is,           ")
+print("Now i will ask some questions so we can decide which direction , {dogName}, should take")
+    
