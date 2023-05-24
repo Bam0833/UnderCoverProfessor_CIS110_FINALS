2f833c12a9ef444b161f2bc630e134f79de94802
Brandon Morgan <63347492+Bam0833@users.noreply.github.com>
2023-05-24 02:39:01 -0400

    Add files via upload
    revision

--- # Instructions for CIS110 FINAL projext1.py
+++ # Instructions for CIS110 FINAL projext1.py
@@ -4,10 +4,12 @@ print("or runaway to a world he has never seen.")
 print("Listed is a series of questions, when you have your chosen input please press enter for each one.")
 print("Continue? Please press the ENTER key")
 print(f"\nWould you like to continue? Please type yes or no:       ")
-keepPlaying = "yes"
+keepPlaying = "y"
 while (len(keepPlaying)) == "no":
  input("GAME OVER")
 #STORY START POINT
+
+
 print("Welcome to Mr. Woofles super secret undercover mission adventure. I am going to ask some questions i need answered so we can help him on his journey and get the paper turned in.")
 userName = input("\nWhat is your name?           ")
 while userName == "0":
@@ -29,32 +31,36 @@ else:
  chosenPath = ["'pathA' or 'pathB'"]
  chosenPath = input(f"\nPlease type your choice, 'pathA', for continue or 'pathB' to run away:       ")
 while chosenPath not in ["pathA" , "pathB"]:
- print("Please make a selection")
+    chosenPath == input("Please choose a path:   ")
+
 while chosenPath == "pathB":
  pathB == input(f"\nWhere you gonna get your kibbles at now Mr Woofles, He never abandoned us! GAME OVER")
-if chosenPath == "pathA":
- pathA == input(f"\nNext we got to infiltrate the web to get this paper submitted, press any key to continue")
-
-keepGoing = "y"
-chooseTool = ["'superTool' , 'superToolA' , superToolB' "]
+ #Tool selections
+ keepWoofin == input("Please enter yes or no")
 superTool = "office"
 superToolA = "coloring pencils and scrap paper"
-superToolB = "ink and paw prints"
 print("We have a wide variety of tools to accomplish this task, Please pick a tool to use please input an option")
 print(f"superTool = {superTool}")
 print(f"superToolA = {superToolA}")
-print(f"superToolB = {superToolB}")
-chooseTool == input(f"Please input one tool :superTool,superToolA,supertoolB:         ")
-if chooseTool == "":
- chooseTool == input("Please select a tool ")
-if chooseTool =="superToolA":
+chooseTool = ["superTool" , "superToolA" ]
+chooseTool == input(f"Please input one tool,superTool or superToolA   ")
+while len(chooseTool) == "0":
+    chooseTool == input("please select a tool!!: ")
+    if chooseTool == ("superTool"):
+        chooseTool == input("Typical, but the best choice by far.Lets start writing, We dont have long. Continue? Input yes or no:       ")
+        while chooseTool == "no":
+            print("Shoulda seen me and Mr Woofles through, Keep woofin anyway, GAME OVER")
+    else:
+        print("Knew you couldnt let Col' Mcfluff down!, Now lets finish the paper and get it turned in!")
+while len(chooseTool) == "superToolA":
          chooseTool == input(f"\nWe draft the paper with the coloring pencils and our scrap paper had some takeout menu printed on the back,")
          chooseTool == input(f"needless to say our paper was failed on site, GAME OVER")
-if chooseTool == "superToolB":
-          chooseTool == input(f"Turns out the head of the college cant read puppy print, Who knew? GAME OVER")
-if chooseTool == "superTool":
-  chooseTool == input(f"\nGood choice , {userName}, Lets begin the paper!")
+if chooseTool == (superTool):
+    chooseTool == input("\nGood choice,{userName}, Lets write the paper!!    ")
 print("We spend hours writing the paper, then find our way to the head of college's main office ahead of the teacher. We turn the paper in and get caught because the paper smells of pure kibbles and bits. We tried! GAME OVER")
 print("The good news is Mr Woofles is no longer undercover and at home safe. Bad news is i failed this class but had a brave puppy to atleast try to save me")
 print("The End")
+if chooseTool == (superToolA):
+    chooseTool == input("We completed the paper and get it around the teacher, He fails us on spot for the very informative picture Mr Barksworthe drew while distracted, We also get home and get grounded for taking our sisters coloring pencils, Thanks Woofles!! GAME OVER")
+
 
